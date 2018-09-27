8baab4a134e8b6eaf331fea764658158d8c608ee
Mariya <mariyawhitcomb@gmail.com>
2018-09-27 19:23:13 -0400

    created the form and added primary logic

--- parentheses/src/App.js
+++ parentheses/src/App.js
@@ -1,18 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Valid from "./Valid";
+import Invalid from "./Invalid";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      code: "",
+      valid: null
+    };
+  }
+  handleChange = e => {
+    this.setState({ code: e.target.value });
+  };
+  checkParentheses = () => {
+    var parenthesesValid;
+    var parentheses = [];
+    const charachters = ["{", "}", "[", "]", "(", ")"];
+    var codeArray = this.state.code.split("");
+    for (let i = 0; i < codeArray.length; i++) {
+      var index = charachters.indexOf(codeArray[i]);
+      if (index >= 0) {
+        if (index % 2 === 0) {
+          parentheses.push(codeArray[i]);
+        } else if (parentheses.length === 0) {
+          this.setState({
+            valid: false
+          });
+          document.body.classList.remove("valid");
+          document.body.classList.add("invalid");
+          return parenthesesValid;
+        } else if (
+          charachters[index - 1] === parentheses[parentheses.length - 1]
+        ) {
+          parentheses.pop();
+        }
+      }
+    }
+    if (parentheses.length === 0) {
+      this.setState({ valid: true });
+      document.body.classList.remove("invalid");
+      document.body.classList.add("valid");
+    } else {
+      this.setState({ valid: false });
+      document.body.classList.remove("valid");
+      document.body.classList.add("invalid");
+    }
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.checkParentheses();
+  };
+  whichCOmponent = () => {
+    if (this.state.valid) {
+      return <Valid />;
+    } else if (this.state.valid == false) {
+      return <Invalid />;
+    }
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Code Validator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label>Paste code to validate</label>
+          <textarea
+            name="code"
+            onChange={this.handleChange}
+            className="form-text"
+          />
+          <input type="submit" value="Submit" className="form-submit" />
+        </form>
+        {this.whichCOmponent()}
       </div>
     );
   }
---
+++ parentheses/src/Valid.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Valid extends Component {
+  render() {
+    return (
+      <div>
+        <p>VALID</p>
+      </div>
+    );
+  }
+}
